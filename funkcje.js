db06d9d2a427537d1e20a2a8846c6ee31a9f276e
Slo-2d-16-2021 <66208199+Dominika275@users.noreply.github.com>
2022-02-04 12:59:45 +0100

    Add files via upload

---
+++ funkcje.js
@@ -0,0 +1,72 @@
+var plik1 = "<img src=\"zdj1.jpg\" >";
+var plik2 = "<img src=\"zdj2.jpg\" >";
+var plik3 = "<img src=\"zdj3.jpg\" >";
+var plik4 = "<img src=\"zdj4.jpg\" >";
+var plik5 = "<img src=\"zdj5.jpg\" >";
+
+function z1()
+{
+	document.getElementById("id1").innerHTML="Zeszyt";
+	document.getElementById("id2").innerHTML= plik1;
+}
+
+
+
+function z2()
+{
+	document.getElementById("id1").innerHTML="Odpowiedź brzmi:";
+	document.getElementById("id2").innerHTML= "";
+}
+
+function z3()
+{
+	document.getElementById("id3").innerHTML="Cyrk";
+	document.getElementById("id4").innerHTML= plik2;
+}
+
+function z4()
+{
+	document.getElementById("id3").innerHTML="Odpowiedź brzmi:";
+	document.getElementById("id4").innerHTML= "";
+}
+
+
+function z5()
+{
+	document.getElementById("id5").innerHTML="Chrząszcz";
+	document.getElementById("id6").innerHTML= plik3;
+}
+
+
+function z6()
+{
+	document.getElementById("id5").innerHTML="Odpowiedź brzmi:";
+	document.getElementById("id6").innerHTML= "";
+}
+
+ 
+
+function z7()
+{
+	document.getElementById("id7").innerHTML="Deszcz";
+	document.getElementById("id8").innerHTML= plik4;
+	}
+
+function z8()
+{
+	document.getElementById("id7").innerHTML="Odpowiedź brzmi:";
+	document.getElementById("id8").innerHTML= "";
+}
+
+function z9()
+{
+	document.getElementById("id9").innerHTML="Schody";
+	document.getElementById("id10").innerHTML= plik5;
+}
+
+function z10()
+{
+	document.getElementById("id9").innerHTML="Odpowiedź brzmi:";
+	document.getElementById("id10").innerHTML= "";
+}
+
